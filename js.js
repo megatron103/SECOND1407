2b955db0b39c3120ac3df71194a60d6aa93bdf79
1407 <huyetvuongluu@gmail.com>
2025-03-17 20:44:06 +0700

    Update js.js

--- js.js
+++ js.js
@@ -13,7 +13,7 @@ updateClock();
     if( x === 'admin' && y === 'gggg') 
         return "chuan";
     else
-        return "có cc";
+        return "sai";
   }
   document.getElementById('login').addEventListener("click", function(event) {
     event.preventDefault(); 
@@ -26,10 +26,38 @@ document.getElementById("login").addEventListener("click",function(){
 
     if( res === "chuan"){
         window.open( "https://megatron103.github.io/FIRST1407/","_blank");
+        document.getElementById("load2").style.display ="none";
     }
     else{ 
-        alert("conmenos");
+       document.getElementById("load2").style.display ="flex";
+       typeEffect();
     }
-})
+});
+
+const text = `#include<bits/stdc++.h>
+using namespace std;
+int slidingwindow( vector<int> &a,
+int &k,int &n,int &start,int &en)
+{int s = 0; start = 0;en = k-1;for(int i = 0; i < k; i++)
+ s+=a[i];int kq = s;for(int i = 0; i< n; i++){ s-=a[i];
+s+= a[i+k];if( s > kq ) {kq = s;start = i-k +1;en = i;}
+    }return kq;
+}\n `;
+
+let i = 0;
+function typeEffect() {
+    if (i < text.length) {
+        let char = text.charAt(i);
+        if (char === "\n") {
+            document.getElementById("text").innerHTML += "<br>";
+        } else {
+            document.getElementById("text").innerHTML += char;
+        }
+        i++;
+        setTimeout(typeEffect, 40);
+    } else {
+        document.getElementById("text").style.borderRight = "none";
+    }
+};
 
 
